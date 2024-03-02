01c9a65c517657ed7a391ef9d95af33d9e4e210a
parth adhikari <parthadhikari1812@gmail.com>
2024-03-02 17:13:42 +0530

    added empty component

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,27 @@
+"use client";
 import React from "react";
+import EmptyOrg from "./_components/emptyOrg";
+import { useOrganization } from "@clerk/nextjs";
+import BoardList from "./_components/boardList";
 
-const Dashboard = () => {
-  return <main>Dashboard Root page</main>;
+interface DashboardPageParams {
+  searchParams: {
+    search?: string;
+    favourites?: string;
+  };
+}
+
+const Dashboard = ({ searchParams }: DashboardPageParams) => {
+  const { organization } = useOrganization();
+  return (
+    <main className="flex-1 h-[calc(100%-80px)] p-6">
+      {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList orgId={organization.id} query={searchParams} />
+      )}
+    </main>
+  );
 };
 
 export default Dashboard;
